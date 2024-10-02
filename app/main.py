743f565dd0f7a9d1714017507cd9b728e189127e
ben-cuff <bencuff14@gmail.com>
2024-10-02 13:28:53 -0400

    started db and api work

--- app/main.py
+++ app/main.py
@@ -1,8 +1,45 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends
 from starlette.responses import RedirectResponse
+from pydantic import BaseModel
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+from typing import Annotated
 
 app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+
+class UserCreate(BaseModel):
+    name: str
+    email: str
+    password: str
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
 
 @app.get("/")
 def read_root():
     return RedirectResponse("https://www.virtualpapertrading.com")
+
+
+@app.post("/users/")
+def create_user(user: UserCreate, db: db_dependency):
+    existing_user = (
+        db.query(models.User).filter(models.User.email == user.email).first()
+    )
+
+    if existing_user:
+        raise HTTPException(status_code=404, detail="user already exists")
+    
+    new_user = models.User(name=user.name, email=user.email, hashed_password=user.password)
---
+++ app/models.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Column, Integer, String, DECIMAL, TIMESTAMP, ForeignKey, func
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Portfolio(Base):
+    __tablename__ = "portfolio"
+
+    portfolio_id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("user.user_id"))
+    ticker_symbol = Column(String(10))
+    shares_owned = Column(DECIMAL(10, 2))
+    average_price = Column(DECIMAL(10, 2))
+    last_transaction = Column(TIMESTAMP, server_default=func.now())
+
+    user = relationship("User", back_populates="portfolio")
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    user_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
+    portfolio = relationship(
+        "Portfolio", back_populates="user", cascade="all, delete-orphan"
+    )
