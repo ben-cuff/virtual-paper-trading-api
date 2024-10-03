812493007c9b3cb9f6afff2ac39efa60f5995a8e
ben-cuff <bencuff14@gmail.com>
2024-10-02 22:15:51 -0400

    added transactions route

--- app/models.py
+++ app/models.py
@@ -5,18 +5,21 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-class Transaction(Base):
-    __tablename__ = "transaction"
+class User(Base):
+    __tablename__ = "user"
 
-    transaction_id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("user.user_id"))
-    ticker_symbol = Column(String(10))
-    shares_quantity = Column(DECIMAL(10, 2))
-    price = Column(DECIMAL(10, 2))
-    transaction_type = Column(String(4))
-    time = Column(TIMESTAMP, server_default=func.now())
+    user_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    balance = Column(DECIMAL(12, 3), default=100000.000)
 
-    user = relationship("User", back_populates="transactions")
+    portfolio = relationship(
+        "Portfolio", back_populates="user", cascade="all, delete-orphan"
+    )
+    transactions = relationship(
+        "Transaction", back_populates="user", cascade="all, delete-orphan"
+    )
 
 
 class Portfolio(Base):
@@ -32,19 +35,15 @@ class Portfolio(Base):
     user = relationship("User", back_populates="portfolio")
 
 
-class User(Base):
-    __tablename__ = "user"
+class Transaction(Base):
+    __tablename__ = "transaction"
 
-    user_id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    balance = Column(DECIMAL(12, 3), default=100000.000)
+    transaction_id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("user.user_id"))
+    ticker_symbol = Column(String(10))
+    transaction_type = Column(String(10))
+    shares_quantity = Column(DECIMAL(10, 2))
+    price = Column(DECIMAL(10, 2))
+    time = Column(TIMESTAMP, server_default=func.now(), onupdate=func.now())
 
-    portfolio = relationship(
-        "Portfolio", back_populates="user", cascade="all, delete-orphan"
-    )
-
-    transaction = relationship(
-        "Transaction", back_populates="user", cascade="all, delete-orphan"
-    )
+    user = relationship("User", back_populates="transactions")
