6d40aa0203c4fc0efe7417b05ce2b49ef4f9bf06
ben-cuff <bencuff14@gmail.com>
2024-10-08 14:30:28 -0400

    more trying to deploy

--- app/api.py
+++ app/api.py
@@ -78,7 +78,7 @@ api_key_dependency = Depends(verify_api_key)
 
 @app.get("/")
 def read_root():
-    return RedirectResponse("https://www.virtualpapertrading.com")
+    return RedirectResponse(url="/docs")
 
 
 @app.get(
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from mangum import Mangum
-from app.api import app
+import uvicorn
 
-handler = Mangum(app)
+if __name__ == "__main__":
+    uvicorn.run("app.api:app", host="127.0.0.1", port=8000, reload=True)
